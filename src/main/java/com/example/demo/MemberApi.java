1bf51e88ae48fb7f95f8fc9e76f3d48e14b749b1
andrew <umanking@gmail.com>
2020-07-29 06:46:24 +0900

    feat: 회원 조회 API 구현

--- src/main/java/com/example/demo/MemberApi.java
+++ src/main/java/com/example/demo/MemberApi.java
@@ -6,6 +6,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * @since 2020-07-28
  */
@@ -16,19 +19,48 @@ public class MemberApi {
     private final MemberRepository memberRepository;
     private final MemberService memberService;
 
-    @PostMapping("/api/v1/member")
+    @GetMapping("/api/v1/members")
+    public List<Member> findMembersV1() {
+        return memberRepository.findAll();
+    }
+
+    @GetMapping("/api/v2/members")
+    public Result<MemberDto> findMembersV2() {
+        List<Member> allMembers = memberRepository.findAll();
+        List<MemberDto> collect = allMembers.stream()
+                .map(member -> new MemberDto(member.getName()))
+                .collect(Collectors.toList());
+
+        return new Result(collect, collect.size());
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        T data;
+        int count;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        String name;
+    }
+
+
+    @PostMapping("/api/v1/members")
     public CreateMemberResponse createMemberV1(@RequestBody @Validated Member member) {
         return new CreateMemberResponse(memberRepository.save(member).getId());
     }
 
-    @PostMapping("/api/v2/member")
+    @PostMapping("/api/v2/members")
     public CreateMemberResponse createMemberV2(@RequestBody @Validated CreateMemberRequest request) {
         Member member = new Member();
         member.setName(request.getName());
         return new CreateMemberResponse(memberRepository.save(member).getId());
     }
 
-    @PutMapping("/api/v2/member/{id}")
+    @PutMapping("/api/v2/members/{id}")
     public UpdateMemberResponse updateMember(@PathVariable Long id, @RequestBody UpdateMemberRequest request) {
         // command와 query 구분
         // update를 하고 반환하지 않고, 새롭게 조회한다.
